aa2fe5ab2b0df1162486524eacbeb230acb42232
Rinor Sadiku <rinor.sadiku2@gmail.com>
2019-05-19 20:05:34 +0200

    test commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // Passport config
 require('./services/passport');
 
-// Wire up middlwares for passport
+// Wire up middlwares for passport ussage
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
