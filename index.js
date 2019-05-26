01c4aae6d3c10883bf60b3fd9bc80389a9f5b4ed
Rinor Sadiku <rinor.sadiku2@gmail.com>
2019-05-26 11:48:02 +0200

    added static file serve

--- index.js
+++ index.js
@@ -34,13 +34,18 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-	res.send({ promvote: 'says hi' });
-});
-
 // Authentication route handlers
 require('./routes/authRoutes')(app);
 require('./routes/voteRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
